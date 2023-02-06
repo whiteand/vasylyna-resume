c8d48091b4884759b094079b621c0b4f86a8401a
whiteand <andrewbeletskiy@gmail.com>
2023-02-06 21:51:04 +0200

    implemented basic text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,91 +1,240 @@
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from './page.module.css'
+import React from "react";
+import s from "./page.module.scss";
 
-const inter = Inter({ subsets: ['latin'] })
+function Header() {
+  return (
+    <header>
+      <h1>Vasylyna Biletska</h1>
+      <div>Junior Data Analyst</div>
+    </header>
+  );
+}
+
+function Pair({
+  title,
+  children,
+}: {
+  title: string;
+  children: React.ReactNode;
+}) {
+  return (
+    <div>
+      <label>{title}</label>
+      <div>{children}</div>
+    </div>
+  );
+}
+
+function Contacts() {
+  return (
+    <section>
+      <Pair title="Address">Lviv</Pair>
+      <Pair title="Email">
+        <a href="mailto:vasylynabiletskaa@gmail.com">
+          vasylynabiletskaa@gmail.com
+        </a>
+      </Pair>
+      <Pair title="Links">
+        <a href="linkedin.com/in/biletskaaa"></a>
+      </Pair>
+    </section>
+  );
+}
+
+function ResumeSection({
+  number,
+  title,
+  children,
+}: {
+  number: number;
+  title: string;
+  children: React.ReactNode;
+}) {
+  return (
+    <section>
+      <h3>
+        {number} {title}
+      </h3>
+      {children}
+    </section>
+  );
+}
+
+function Profile({ number }: { number: number }) {
+  return (
+    <ResumeSection number={number} title="Profile">
+      <ul>
+        <li>
+          Have reduced the time spent on the existing reports by 8-10 times,
+          automating a lot of manual work.
+        </li>
+        <li>
+          Implemented Looker (Google Data Studio) in work and developed
+          automated dashboards. Redesigned methods of reporting and simplified
+          data collection.
+        </li>
+        <li>Optimized website event tracking</li>
+        <li>
+          My scientific area of interest is recommender systems and their impact
+          on people. That is why my university thesis project was related to the
+          construction of recommendation systems (alogrithms and metrics) and
+          some exploratory data analysis using Pythong libraries.
+        </li>
+      </ul>
+    </ResumeSection>
+  );
+}
+function HistoryItem({
+  range,
+  title,
+  children,
+}: {
+  range: string;
+  title: string;
+  children?: React.ReactNode;
+}) {
+  return (
+    <section>
+      <label>{range}</label>
+      <main>
+        <h4>{title}</h4>
+        {children}
+      </main>
+    </section>
+  );
+}
+
+function EmploymentHistory({ number }: { number: number }) {
+  return (
+    <ResumeSection number={number} title="Employment History">
+      <HistoryItem
+        range="10/2021 - Present"
+        title="Web Marketing Analyst at ams.agency"
+      >
+        <p>
+          At my current job. I work in a digital agency with many clients, and
+          for each of them, I perform analythical tasks such as:
+        </p>
+        <ol>
+          <li>
+            The definition of critical metrics and commercial conversions, which
+            uses to monitor the effectiveness of advertising campaigns.
+          </li>
+          <li>
+            Setting up events through Google Tag Manager to track conversions to
+            the Google Analytics system and tracking heat maps.
+          </li>
+          <li>
+            Direct data analytics, monthly and ad-hoc, which include:
+            <ul>
+              <li>
+                identification of the most and least effective campaigns and
+                creatives for each of them
+              </li>
+              <li>analysis of user behavior on the site</li>
+              <li>
+                analysis of the quality of traffic from each advertising
+                campaign
+              </li>
+              <li>identifying causes of abrupt changes in metrics</li>
+              <li>conclusions and recommendations for the client</li>
+            </ul>
+          </li>
+          <li>Creating new reports using the Looker tool with SQL queries</li>
+        </ol>
+      </HistoryItem>
+      <HistoryItem
+        range="03/2019 - 09/2019"
+        title="Assistant Research Analyst at Top Lead"
+      >
+        <p>
+          In this position, I worked with data &quot;manually&quot; and my job
+          was to:
+        </p>
+        <ol>
+          <li>search for data on a given topic from verified sources.</li>
+          <li>
+            gathering, processing, and organizing this data in Excel and their
+            recheck.
+          </li>
+          <li>
+            visualize processed data in PowerPoint for a designer of informative
+            FB posts.
+          </li>
+        </ol>
+      </HistoryItem>
+    </ResumeSection>
+  );
+}
+function Education({ number }: { number: number }) {
+  return (
+    <ResumeSection number={number} title="Employment History">
+      <HistoryItem
+        range="Sep 2017 - Jun 2021"
+        title="Kyiv National Econommics University"
+      >
+        <p>Economic cybernetics</p>
+      </HistoryItem>
+    </ResumeSection>
+  );
+}
+function Progress({
+  value,
+  max,
+  className,
+}: {
+  value: number;
+  max: number;
+  className?: string;
+}) {
+  const dots = [];
+  for (let i = 0; i < max; i++) {
+    dots.push(<div key={i} className={i < value ? s.active : s.inactive} />);
+  }
+  return <div className={className}>{dots}</div>;
+}
+
+function Skill({ title, value }: { title: string; value: number }) {
+  return (
+    <div>
+      <label>{title}</label>
+      <Progress value={value} max={5} />
+    </div>
+  );
+}
+
+function Skills({ number }: { number: number }) {
+  return (
+    <ResumeSection number={number} title="Employment History">
+      <Skill title="SQL" value={5} />
+      <Skill title="Looker (BI tool)" value={5} />
+      <Skill title="Microsoft Excel" value={5} />
+      <Skill title="Python" value={2} />
+      <Skill title="Detail Oriented" value={5} />
+      <Skill title="Tableau" value={3} />
+      <Skill title="Google Analytics" value={4} />
+      <Skill title="Data Analysis" value={3} />
+    </ResumeSection>
+  );
+}
+function Languages({ number }: { number: number }) {
+  return (
+    <ResumeSection number={number} title="Employment History">
+      <Skill title="English" value={3} />
+    </ResumeSection>
+  );
+}
+
+const SECTIONS = [Profile, EmploymentHistory, Education, Skills, Languages];
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
-        </div>
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+    <main className={s.main}>
+      <Header />
+      <Contacts />
+      {SECTIONS.map((Section, ind) => (
+        <Section number={ind + 1} key={Section.name} />
+      ))}
     </main>
-  )
+  );
 }
